c1cc8ad11f821a545c2563c969ea6fc007f7cd97
Agrawal Rajat <72139083+Agrawal-Rajat@users.noreply.github.com>
2023-11-23 18:13:57 +0000

    conversion of data types

--- 01_basics/03_conversion_datatypes.js
+++ 01_basics/03_conversion_datatypes.js
@@ -4,4 +4,24 @@ let id2 = null
 let id3 = NaN
 let id4 = false
 let idInNumber = Number(id4)
-console.log(idInNumber);
+//console.log(idInNumber)
+// string - NaN
+// null -  0
+// NaN - NaN
+// false - 0
+// true - 1
+
+let isLoggedin = "rajat"
+let booleanIsLoggedIn = Boolean(isLoggedin)
+//console.log(booleanIsLoggedIn)
+
+// 1 - true
+// 0 - false
+// "" - false
+// "rajat" - true
+
+let num = 32
+let someNumber = String(num)
+console.log(typeof someNumber);
+
+// yaha convert karte samay Number, String, Boolean ye sab ka capital leeter hai
