982f5150cafe43a6135e16973b238df0cbd2c9fe
Agrawal Rajat <72139083+Agrawal-Rajat@users.noreply.github.com>
2023-11-23 14:19:40 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 2121
+let accountEmail = "rajat@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState
+
+// accountId = 2        //this is not allowed
+
+accountEmail = "rajat@yahoo.com"
+accountPassword = "2222"
+accountCity = "Banglore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
