e4f377d352b5ca1148e8a793640a42263e753f1a
Agrawal Rajat <72139083+Agrawal-Rajat@users.noreply.github.com>
2023-11-23 18:37:08 +0000

    operations

--- 01_basics/03_conversion_datatypes.js
+++ 01_basics/03_conversion_datatypes.js
@@ -22,6 +22,31 @@ let booleanIsLoggedIn = Boolean(isLoggedin)
 
 let num = 32
 let someNumber = String(num)
-console.log(typeof someNumber);
+//console.log(typeof someNumber);
 
 // yaha convert karte samay Number, String, Boolean ye sab ka capital leeter hai
+
+//console.log(2**2) // 2 ki power 2
+//console.log(2%3); // returns the remainder
+
+let str1 = "Hello "
+let str2 = "World"
+let str3 = str1 + str2
+//console.log(str3); // returns hello world
+
+// console.log("1" + 2); // returns 12
+// console.log("1" + "2"); //return 12
+// console.log("1" + 2 + 2); //return 122
+// console.log(1 + 2 + "2"); // returns 32
+
+// console.log(true); // returns true
+// console.log(+true) // retruns 1 
+// console.log(true+); // gives an error
+// console.log(+""); // returns 0
+
+// let num3, num4, num5
+// num3 = num4 = num5 = 2+2 //also can be initialised
+
+// let num5 = 34;
+// num5++ 
+// console.log(num5); // returns 35
