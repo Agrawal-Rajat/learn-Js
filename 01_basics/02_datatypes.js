4d65b3120e34ccd4e330ddee389734f2d1a9112f
Agrawal Rajat <72139083+Agrawal-Rajat@users.noreply.github.com>
2023-11-23 14:19:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // this line will help to treat all js code as newer version
+
+//alert(3 + 3) // this will give error as we are using node js not browser
+
+let name = " Rajat";
+let Id  = 21;
+let isLoggedIn = false;
+let issue = null;
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
