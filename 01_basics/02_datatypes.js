39f46cffdaa19d40dfaa5b07ebe7dc62e7f2ab38
Agrawal Rajat <72139083+Agrawal-Rajat@users.noreply.github.com>
2023-11-24 18:19:20 +0000

    stack and heap memory

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -34,5 +34,21 @@ let obj = {
 
 //3. function
 function myFunc() {
-    console.log("hello world");
+   // console.log("hello world");
 }
+
+let name1 = 23;
+let name2  = name1
+name2 = 45
+// console.log(name2); // 45 as changes are made in copy in stack
+// console.log(name1); // 23 as its original value is not changed
+
+const userOne = {
+    email : "raj@gmail.com",
+    id : 21
+}
+
+let userTwo = userOne
+userTwo.email = "rdf@gmail.com"
+console.log(userTwo.email); //rdf as orginal value is changed
+console.log(userOne.email); //rdf as orginal value is changes cause in heap we dont create a copy just take a refernece
