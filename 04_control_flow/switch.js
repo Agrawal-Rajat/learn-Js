60926d4723a5044f3da6b18857431f0e7f6e56b8
Agrawal Rajat <72139083+Agrawal-Rajat@users.noreply.github.com>
2023-12-25 13:07:04 +0000

    switch statements

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,23 @@
+const month = 4;
+switch(month){
+    case 1 : 
+    console.log("jan");
+    break;
+    case 2 : 
+    console.log("feb");
+    break;
+    case 3 : 
+    console.log("march");
+    break;
+    case 4 : 
+    console.log("april");
+    break;
+    case 5 : 
+    console.log("may");
+    break;
+
+    default : 
+    console.log("none");
+}
+
+// agar break hata denge toh uska baad ke sare case print honge except default
