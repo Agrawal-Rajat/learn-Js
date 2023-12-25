bb1d89239d4fd1622874cbbe7ec5e3496b6fa2aa
Agrawal Rajat <72139083+Agrawal-Rajat@users.noreply.github.com>
2023-12-25 16:59:25 +0000

    for loop, break and continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,27 @@
+// for loops
+//syntax --
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// } 
+// agar iske bahar dusri loop likhenge toh usme bhi hum index hee use kar sakte hai cause vo index toh ussi scope mei shuru hua aur khatam ho gya 
+
+
+// break statement
+// for(let i = 1; i < 10; i++){
+//     if(i == 5){
+//         break; // 1 2 3 4
+//         // it will come out of the loop 
+//     }
+//     console.log(i);
+// }
+
+// continue statement
+// for(let i = 1; i < 10; i++){
+//     if(i == 5){
+//         continue; // 1 2 3 4 6 7 8 9 
+//         // it will skip this iteration where i == 5, abd directly movie to next iteration
+//     }
+//     console.log(i);
+// }
+
