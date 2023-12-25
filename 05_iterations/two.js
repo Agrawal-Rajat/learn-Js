58fa4ff3c947296d17eabcdeffee5fc009978305
Agrawal Rajat <72139083+Agrawal-Rajat@users.noreply.github.com>
2023-12-25 17:44:26 +0000

    while, do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+// while loop
+// let i = 1;
+// while(i <= 10){
+//     console.log(i);
+//     i++
+// } 
+
+//do-while loop
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i <= 10);
