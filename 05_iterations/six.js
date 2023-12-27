6b83a9d8b2c09a898b47a91f89c0b2f6f4211712
Agrawal Rajat <72139083+Agrawal-Rajat@users.noreply.github.com>
2023-12-27 16:35:19 +0000

    filter method

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -30,3 +30,35 @@ const myObs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //         console.log(item); // it will print same but no in array format
 //     }
 //  })
+
+// using filter to filter out the results that we want in the array of objects 
+const books = [
+    {
+        bookName : "harry porter",
+        publishDate : 1999,
+        edition : 2023
+    },
+    {
+        bookName : "2 states",
+        publishDate : 2000,
+        edition : 2022
+    },
+    {
+        bookName : "witch craft",
+        publishDate : 1976,
+        edition : 2012
+    },
+    {
+        bookName : "money power",
+        publishDate : 2002,
+        edition : 2023
+    }
+]
+
+// now geeting those book records which are published after 2000
+//const myBooks = books.filter( (bks) => bks.publishDate >= 2000)
+//console.log(myBooks);
+
+// now getting those books whose edition are of 2023
+const myBooks = books.filter( (bks) => {return bks.edition === 2023})
+console.log(myBooks);
